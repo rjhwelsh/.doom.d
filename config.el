089cd7f7f653481e4b9da3f1ff02b2528995b94d
Roger Welsh <rjhwelsh+git@posteo.net>
2021-11-01 18:43:32 +1300

    config: Add emoji support to emacs

--- config.el
+++ config.el
@@ -46,6 +46,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
+(setq emojify-display-style 'unicode)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
