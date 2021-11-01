bd18fbf8035d9ed46ad1a1d20bf7e1a382bac0be
Roger Welsh <rjhwelsh+git@posteo.net>
2021-11-01 18:42:35 +1300

    modules/app/matrix: Replace matrix-client w/ ement

--- modules/app/matrix/config.el
+++ modules/app/matrix/config.el
@@ -1,6 +1,9 @@
 ;;; app/matrix/config.el -*- lexical-binding: t; -*-
 
-(use-package! matrix-client
-  :init
-  (setq
-   matrix-client-save-token t))
+;; (use-package! matrix-client
+;;   :init
+;;   (setq
+;;    matrix-client-save-token t))
+
+(use-package! plz)
+(use-package! ement)
--- modules/app/matrix/packages.el
+++ modules/app/matrix/packages.el
@@ -1,9 +1,22 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/matrix/packages.el
 
-(package! matrix-client
-  :pin "d2ac55293c96d4c95971ed8e2a3f6f354565c5ed"
+;; (package! matrix-client
+;;   :pin "d2ac55293c96d4c95971ed8e2a3f6f354565c5ed"
+;;   :recipe
+;;   (:host github
+;;    :repo "alphapapa/matrix-client.el")
+;;   )
+
+(package! ement
+  :pin "c951737dc855604aba389166bb0e7366afadc533"
   :recipe
   (:host github
-   :repo "alphapapa/matrix-client.el")
+   :repo "alphapapa/ement.el")
   )
+
+(package! plz
+  :pin "7e456638a651bab3a814e3ea81742dd917509cbb"
+  :recipe
+  (:host github
+   :repo "alphapapa/plz.el"))
