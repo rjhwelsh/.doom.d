94f16f68f6a9eb376721ae8270a779a76e176831
Roger Welsh <rjhwelsh+git@posteo.net>
2022-06-10 08:28:50 +1200

    compat: comment out mu4e and old org
    this disables rjh/solar which may cause
    some sexps to not work

--- rjh-compat.el
+++ rjh-compat.el
@@ -17,16 +17,20 @@
 
 ;; The following steps are required outside of Emacs
 ;; Symlink ~/.emacs.doom/org          -> ~/domestic/static/org-local/
-(after! org
-  	;; Load literate configuration
-	(mapc (lambda (f) (and
-                           (file-exists-p f)
-                           (org-babel-load-file f)))
-              rjh-org-mode-config-list)
-        )
+;; (after! org
+;;   	;; Load literate configuration
+;; 	(mapc (lambda (f) (and
+;;                            (file-exists-p f)
+;;                            (org-babel-load-file f)))
+;;               rjh-org-mode-config-list)
+;;         )
 ;; Email
 ;;
 ;; :email mu4e
+;; (after! mu4e
+;; 	(org-babel-load-file (concat rjh-old-init-dir    "djcb/mu4e.org"))
+;; 	)
+
 
 
 ;; --------------------------------------------------------------------------------
